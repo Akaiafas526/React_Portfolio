e9e8d5aba98460abd0452f76eee9b8590be7f352
Andreas Kaiafas <akaiafas526@gmail.com>
2022-04-24 18:45:57 -0400

    wrote another paragraph in the about section

--- src/components/Pages/About/about.js
+++ src/components/Pages/About/about.js
@@ -1,9 +1,22 @@
 import React, { useState } from "react";
 
 export default function About() {
-        return (
-            <div className="about">
-                about
-            </div>
-        )
+  return (
+    <div className="about">
+      <div className="about-header">
+        <h2> About Me </h2>
+      </div>
+
+      <div className="about-text">
+        <p> My name is Andreas Kaiafas and I am junior software developer.</p>
+        <p>
+          {" "}
+          My love and passion for learning has carried me from a career focused
+          around digging deep into the science of food, to an exciting new
+          career engineering our futures software!
+        </p>
+        <p></p>
+      </div>
+    </div>
+  );
 }
