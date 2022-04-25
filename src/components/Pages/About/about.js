8b09f5d232972ae28c2991e9ff634be4fd364451
Andreas Kaiafas <akaiafas526@gmail.com>
2022-04-25 09:27:42 -0400

    created buttons for all footer links, update to text in about section, added div with p tag in conact page

--- src/components/Pages/About/about.js
+++ src/components/Pages/About/about.js
@@ -4,18 +4,29 @@ export default function About() {
   return (
     <div className="about">
       <div className="about-header">
-        <h2> About Me </h2>
+          <img src="./assets/images/profile_pic.png" alt="Profile Picture"></img>
+        <h2> Introduction </h2>
       </div>
 
       <div className="about-text">
-        <p> My name is Andreas Kaiafas and I am junior software developer.</p>
         <p>
           {" "}
-          My love and passion for learning has carried me from a career focused
-          around digging deep into the science of food, to an exciting new
-          career engineering our futures software!
+          Hello my name is Andreas Kaiafas a junior software developer and tester with
+          a passion for creativity and self developement. My love and passion
+          for learning has carried me from a career focused around digging deep
+          into the science of our food and agriculture, to an exciting new
+          adventure engineering our futures software!
         </p>
-        <p></p>
+        <p>
+          I have a few hobbies I enjoy in my freetime. These include creating
+          fun and new recipes to make delicious meals for my family, playing
+          soccer with my friends, riding my bicycle to take in the day and going
+          to the gym. Coding is a newfound passion of mine that has taken me by
+          storm. The feeling you get when immersed in a new and exciting project
+          is much like the same feeling I got when being behind the line in a
+          kitchen. I've found my new love and I am looking forward to pursuing a
+          career in software developement.
+        </p>
       </div>
     </div>
   );
--- src/components/Pages/Contact/contact.js
+++ src/components/Pages/Contact/contact.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
 export default function Contact() {
-        return (
-            <div className="contact">
-                contact
-            </div>
-        )
+  return (
+    <div className="contact">
+      <h2>Please contact me!</h2>
+    </div>
+  );
 }
