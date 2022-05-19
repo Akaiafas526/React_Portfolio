d2b4f9e386db2e0302d0225ff6d72899592a6bff
Andreas Kaiafas <akaiafas526@gmail.com>
2022-05-18 20:59:34 -0400

    centered project titles, imported link icon and changed styling on project page

--- src/components/Pages/Projects/projects.js
+++ src/components/Pages/Projects/projects.js
@@ -1,16 +1,17 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
 
 export default function Projects() {
+  library.add(faLink);
   return (
     <div className="projects">
       <div className="main-project" id="project-1">
         <div className="title-card">
           <h2 className="project-title">SnapShot</h2>
           <a className="repo" href="https://github.com/Akaiafas526/SnapShot">
-        Repository:
-          </a>
-          <a className="deploy" href="https://snapshotban.herokuapp.com/">
-            Deployed:
+            <FontAwesomeIcon className="fa-icon" icon="fa-solid fa-link" />
           </a>
         </div>
       </div>
@@ -18,33 +19,33 @@ export default function Projects() {
         <div className="project-container" id="project-2">
           <div className="title-card">
             <h2 className="project-title">Tech Blog</h2>
-            <a className="repo" href="https://github.com/Akaiafas526/Tech_Blogtechblog">
-            Repository:
-            </a>
-            <a className="deploy" href="https://tech-blog-ak.herokuapp.com/">
-              Deployed:
+            <a
+              className="repo"
+              href="https://github.com/Akaiafas526/Tech_Blogtechblog"
+            >
+              <FontAwesomeIcon className="fa-icon" icon="fa-solid fa-link" />
             </a>
           </div>
         </div>
         <div className="project-container" id="project-3">
           <div className="title-card">
             <h2 className="project-title">Note Taker</h2>
-            <a className="repo" href="https://github.com/Akaiafas526/Note_Taker">
-            Repository
-            </a>
-            <a className="deploy" href="https://note-taker-andreas.herokuapp.com/">
-              Deployed:
+            <a
+              className="repo"
+              href="https://github.com/Akaiafas526/Note_Taker"
+            >
+              <FontAwesomeIcon className="fa-icon" icon="fa-solid fa-link" />
             </a>
           </div>
         </div>
         <div className="project-container" id="project-4">
           <div className="title-card">
             <h2 className="project-title">NBA Stat Website</h2>
-            <a className="repo" href="https://github.com/danchanyoungkim/NBA-Team-Stats">
-              <h5>Repository:</h5>
-            </a>
-            <a className="deploy" href="https://danchanyoungkim.github.io/NBA-Team-Stats/">
-              <h5>Deployed:</h5>
+            <a
+              className="repo"
+              href="https://github.com/danchanyoungkim/NBA-Team-Stats"
+            >
+              <FontAwesomeIcon className="fa-icon" icon="fa-solid fa-link" />
             </a>
           </div>
         </div>
